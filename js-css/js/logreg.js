9203302117f333ab399bcabf6274bbd03121ffd6
MasterCraft-ad <app.arewapulse@gmail.com>
2024-09-30 18:55:44 +0100

    Modification of code

---
+++ js-css/js/logreg.js
@@ -0,0 +1,11 @@
+    function goMain() {
+        window.location.href = '/scr/view/apartment/main.html';
+    }
+
+    function goBack() {
+    window.history.back();
+}
+
+function ExploreHouses() {
+    window.history.back();
+}
